72d02d987ed7b4f1e656a199ab40b3465883037c
Oleksandra <ashchyhorieva@gmail.com>
2022-08-04 22:44:05 +0300

    task-06

---
+++ js/task-06.js
@@ -0,0 +1,35 @@
+const textInput = document.querySelector('input#validation-input');
+console.log(textInput.value.length);
+console.log(typeof textInput.value.length);
+
+const inputLength = document.querySelector('input[data-length="6"]');
+console.log(inputLength.dataset.length);
+console.log(typeof inputLength.dataset.length);
+
+
+textInput.addEventListener('focus', onInputFocus);
+textInput.addEventListener('blur', onInputBlur);
+textInput.addEventListener('change', onInputChange);
+
+function onInputFocus() {
+    console.log(onInputFocus);
+};
+
+function onInputBlur() {
+
+    if (textInput.value.length === Number(inputLength.dataset.length)) {
+        textInput.classList.add('valid');
+        console.log("Added class: valid");
+    } else {
+        textInput.classList.add('invalid');
+        console.log("Added class: invalid");    
+    };
+
+    console.log(onInputBlur);
+};
+
+function onInputChange(event) {
+
+    console.log(event.currentTarget.value.length);
+};
+
