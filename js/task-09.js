7b27a3bd647ab7d1d50351d5f392274a416be691
Oleksandra <ashchyhorieva@gmail.com>
2022-08-06 00:32:00 +0300

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -5,8 +5,14 @@
 Для генерування випадкового кольору використовуй функцію getRandomHexColor.
 */
 
+//function getRandomHexColor() {
+//  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+//}
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 
 const body = document.querySelector('body');
